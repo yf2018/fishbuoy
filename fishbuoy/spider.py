61cbb70adc2d703999e009275207b144d787d9a6
Yang Fei <1328439793@qq.com>
2020-07-26 15:28:35 +0800

    map update

--- fishbuoy/spider.py
+++ fishbuoy/spider.py
@@ -4,16 +4,17 @@
 # @File    : spider.py
 # @Software: Pycharm
 # @Author  : Changan
-
-url = 'http://www.pm25.com/rank.html'
 import pandas
 import time
 import re
 from lxml import etree
 from selenium import webdriver
 import pymysql
-browser = webdriver.Chrome()
+import json
+from urllib.request import urlopen, quote
+import requests
 
+browser = webdriver.Chrome()
 def get_data(sql):
     conn = pymysql.connect('127.0.0.1','root','132441','aqi',charset = 'utf8')
     cur = conn.cursor()
@@ -32,8 +33,19 @@ def get_all_aqi(url):
         province = selector.xpath('/html/body/div[5]/div/div[3]/ul[2]/li[%d]/span[3]/text()' % (i))
         aqi = selector.xpath('/html/body/div[5]/div/div[3]/ul[2]/li[%d]/span[4]/text()' % (i))
         pm25 = selector.xpath('/html/body/div[5]/div/div[3]/ul[2]/li[%d]/span[5]/text()' % (i))
-        sql = "insert into aqi.city_aqi(ranknum,quality,location,province,aqi,pm25)\
-             values("+''.join(ranknum)+","+"'"+''.join(quality)+"'"+","+"'"+''.join(location)+"'"+","+"'"+''.join(province)+"'"+","+''.join(aqi)+","+''.join(pm25)+");"
+        getlnglat(''.join(province)+''.join(location))
+        # sql = "insert into aqi.city_aqi(ranknum,quality,location,province,aqi,pm25)\
+        #      values("+''.join(ranknum)+","+"'"+''.join(quality)+"'"+","+"'"+''.join(location)+"'"+","+"'"+''.join(province)+"'"+","+''.join(aqi)+","+''.join(pm25)+");"
         # sql = "insert into aqi.city_aqi(ranknum,quality,location,province,aqi,pm25)values(%s,%s,%s,%s,%s,%s);"
-        get_data(sql)
-get_all_aqi(url)
+        # get_data(sql)
+
+def getlnglat(address):
+    # address = quote(address) # 由于本文地址变量为中文，为防止乱码，先用quote进行编码
+    base_url = "http://api.map.baidu.com/geocoder?address={address}&output=json&ak=WHEAK2BlGjK2yGy0RD06Ym2nGYGrmzmm".format(address = address)
+    response = requests.get(base_url)
+    answer = response.json()
+    lng = answer['result']['location']['lng']
+    lat = answer['result']['location']['lat']
+    print(address,lng,lat)
+    return 0
+get_all_aqi('http://www.pm25.com/rank.html')
--- fishbuoy/views.py
+++ fishbuoy/views.py
@@ -8,6 +8,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import pymysql
+import requests
 
 def index(request):
     return render(request,'index.html')
@@ -20,15 +21,21 @@ def mapshow(request):
     province = []
     aqi = []
     pm25 = []
+    longitude = []
+    latitude = []
     for list in m_data:
+        m_list = []
         ranknum.append(list[0])
         quality.append(list[1])
         location.append(list[2])
         province.append(list[3])
         aqi.append(list[4])
         pm25.append(list[5])
-    print(location)
-    return render(request,'mapshow.html',{'data':location})
+    # m_address = []
+    大兴安岭
+    for address in location:
+        print(address)
+    return render(request,'mapshow.html',{'data':m_data})
 def get_data(sql):
     conn = pymysql.connect('127.0.0.1', 'root', '132441', 'aqi', charset = 'utf8')
     cur = conn.cursor()
@@ -37,3 +44,16 @@ def get_data(sql):
     cur.close()
     conn.close()
     return results
+def geocode(address):
+    """
+    @ address: 名称字符串
+    @ 返回值：经度，纬度
+    """
+    base_url = "http://api.map.baidu.com/geocoder?address={address}&output=json&ak=WHEAK2BlGjK2yGy0RD06Ym2nGYGrmzmm".format(address=address)
+    response = requests.get(base_url)
+    answer = response.json()
+    longitude = answer['result']['location']['lng']
+    latitude = answer['result']['location']['lat']
+    list = [longitude,latitude]
+    # print(list)
+    return list
